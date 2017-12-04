eee5989c740e955134214ecf85c907f4e7c4cbd5
Simon Zimmermann <simon.zimmermann@rocketmail.com>
2017-12-04 15:47:21 +0100

    OSC package

---
+++ OSCCall.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSCCall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		OSCHandler.Instance.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
